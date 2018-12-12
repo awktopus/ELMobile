c209277206ff34d0ffad796a9c6c9c904a2a4cea
Tracy <awktopus17@gmail.com>
2018-12-11 22:26:42 -0500

    ugh3

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -20,7 +20,7 @@ export class ChatPage {
     {name: 'Susan Li', message: "I just sent the PDF and message to the", time: "3:49PM", status: "unread"},
     {name: 'Charles Yi', message: "When you have time can you check this", time: "2:17PM", status: "unread"},
     {name: 'DuKe Du', message: "Ni shang zhe he wang zhan shi shi wo z", time: "11:53PM", status: "read"},
-    {name: 'Qew Bacca', message: "Hey Tracy!! I'll be coming into the office!", time: "8:33PM", status: "read"}
+    {name: 'Qew Bacca', message: "Hey Tracy!!! I'll be coming into the office!", time: "8:33PM", status: "read"}
   ]
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
