442199b788759fc65694c92bbc325f5034d8f105
Tracy <awktopus17@gmail.com>
2018-12-12 15:58:13 -0500

    ugh23

--- src/pages/channel/channel.module.ts
+++ src/pages/channel/channel.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { ChannelPage } from './channel';
 
 @NgModule({
   declarations: [
-    ChannelPage,
+    
   ],
   imports: [
-    IonicPageModule.forChild(ChannelPage),
+    
   ],
 })
 export class ChannelPageModule {}
